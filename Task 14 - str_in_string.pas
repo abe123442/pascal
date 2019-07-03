e73c389f12ee6976add24fc5c44e07b43752e3af
abe123442 <44077482+abe123442@users.noreply.github.com>
2019-07-04 09:48:24 +1000

    fixed task14

--- Task 14 - str_in_string.pas
+++ Task 14 - str_in_string.pas
@@ -2,23 +2,25 @@ program findStr;
 var
 	str1, str2: string;
 	i, j: Integer;
-	containStr: Boolean;
 begin
 	write('Enter a word: ');
 	readln(str1);
 	write('Enter another word: ');
 	readln(str2);
 
-	for i := 1 to length(str2) do
+	for i := 1 to (length(str2)-length(str1)) do
 	begin
-		for j := length(str1) to length(str2) do
-		begin
-			if str1[j] <> str2[i+j] then
-				containStr := false
-			else 
-				containStr := true;
+	    if str1[1] = str2[i] then
+	    begin
+		    for j := 2 to length(str1) do
+		    begin
+			    if str1[j] <> str2[i+j-1] then
+				    break;
+		    end;
 		end;
 	end;
-	if containStr then writeln('Yes')
-	else writeln('No');
+	if j = length(str1) then
+	    writeln('Yes')
+	else
+	    writeln('No');
 end.
